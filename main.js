f81ac3fcce631b2daec59a359026c96cc2999bf1
ficerman <ficermanlaura@gmail.com>
2023-11-27 13:05:13 +0100

    added search bar and search preview

--- main.js
+++ main.js
@@ -64,3 +64,77 @@ fetch(
       ")"
   })
   .catch(err => console.error(err))
+
+const searchPreview = document.getElementById("searchPreview")
+const searchInput = document.getElementById("searchInput")
+
+searchInput.addEventListener("input", function (evt) {
+  fetch(
+    "https://api.themoviedb.org/3/search/movie?query=" +
+      this.value +
+      "&include_adult=false&language=en-US&page=1",
+    options
+  )
+    .then(response => response.json())
+    .then(response => {
+      searchPreview.innerHTML = `<div class="row">
+<div class="col-sm">
+    <img src="" id="result1" height="200px"></img>
+    <p id="result1Text"></p>
+</div>
+<div class="col-sm">
+
+    <img src="" id="result2" height="200px"></img>
+    <p id="result2Text"></p>
+</div>
+<div class="col-sm">
+
+    <img src="" id="result3" height="200px"></img>
+    <p id="result3Text"></p>
+</div>
+<div class="col-sm">
+
+    <img src="" id="result4" height="200px"></img>
+    <p id="result4Text"></p>
+</div>
+</div>`
+      result1.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/w500" + response.results[0].poster_path
+      )
+      result1Text.innerHTML =
+        response.results[0].title +
+        " (" +
+        response.results[0].release_date.substring(0, 4) +
+        ")"
+      result2.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/w500" + response.results[1].poster_path
+      )
+      result2Text.innerHTML =
+        response.results[1].title +
+        " (" +
+        response.results[1].release_date.substring(0, 4) +
+        ")"
+      result4.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/w500" + response.results[3].poster_path
+      )
+      result4Text.innerHTML =
+        response.results[3].title +
+        " (" +
+        response.results[3].release_date.substring(0, 4) +
+        ")"
+      result3.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/w500" + response.results[2].poster_path
+      )
+      result3Text.innerHTML =
+        response.results[2].title +
+        " (" +
+        response.results[2].release_date.substring(0, 4) +
+        ")"
+    })
+    .then(response => console.log(response))
+    .catch(err => console.error(err))
+})
